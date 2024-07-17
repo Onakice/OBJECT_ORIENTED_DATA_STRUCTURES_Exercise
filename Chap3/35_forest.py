671ee46d68160ad4e46da108fcc5b1aec60c14ce
Onakice <pongsapak.tatongjai+1@gmail.com>
2024-07-17 22:41:21 +0700

    This part is add mushroom i use only one function

---
+++ Chap3/35_forest.py
@@ -0,0 +1,45 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, value):
+        self.items.append(value)
+
+    def size(self):
+        return len(self.items)
+    
+    def seeking(self):
+        if self.size() == 0:
+            return 0
+
+        seen_trees = 0
+        max_height = float('-inf')
+
+        for height in reversed(self.items):
+            if height > max_height:
+                seen_trees += 1
+                max_height = height
+
+        return seen_trees
+    
+    def apply_mushroom_effect(self):
+        for i in range(len(self.items)):
+            if self.items[i] % 2 == 0:
+                self.items[i] = max(1, self.items[i] - 1)
+            else:
+                self.items[i] += 2
+
+S = Stack()
+
+inps = input('Enter Input : ').split(',')
+
+for inp in inps :
+    inp = inp.strip()
+    if inp.startswith('A'):
+        del_head, value = inp.split()
+        S.push(int(value))
+    elif inp == 'B':
+        visible_trees = S.seeking()
+        print(visible_trees)
+    elif inp == 'S':
+        S.apply_mushroom_effect()
